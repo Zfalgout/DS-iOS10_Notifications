0e177a5bd25dc73316c01eb6aa158b7f441b1fc6
Zack Falgout <zfalgout@gmail.com>
2017-06-05 19:34:32 -0500

    Fixed the bug from earlier and wrapped up the lecture.

--- NewNotifications/AppDelegate.swift
+++ NewNotifications/AppDelegate.swift
@@ -52,7 +52,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let favAction = UNNotificationAction(identifier: "fistBump", title: "Da fistBump", options: [])
         let dismissAction = UNNotificationAction(identifier: "dismiss", title: "dismiss", options: [])
         
-        let category = UNNotificationCategory(identifier: "myNotificationCategory", actions[favAction, dismissAction], intentIdentifiers: [], actions: [])
+        let category = UNNotificationCategory(identifier: "myNotificationCategory", actions: [favAction, dismissAction], intentIdentifiers: [])
         UNUserNotificationCenter.current().setNotificationCategories([category])
     }
 
@@ -61,6 +61,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler(.alert)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print("Response recieved for \(response.actionIdentifier)")
+        completionHandler()
     }
     
 }
